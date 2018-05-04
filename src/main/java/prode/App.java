29c2f2138baa55324c0a8d12a1ad7ad41a6c6077
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2018-05-04 10:54:28 -0300

    Update database credentials file

--- src/main/java/prode/App.java
+++ src/main/java/prode/App.java
@@ -4,18 +4,14 @@ import org.javalite.activejdbc.Base;
 
 import prode.User;
 
-
-
 /**
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode?nullNamePatternMatchesAll=true", "franco", "franco");
+public class App {
+    public static void main( String[] args ) {
+        // Improve it and delete user name for db connector
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1/prode?nullNamePatternMatchesAll=true", "lio", "password");
 
         User u = new User();
         u.set("username", "Riquelme");
